d27e3d7b9825e98a97f11fc91b48f907f005c09d
ankush <ankushkawanpure@gmail.com>
2018-01-08 16:29:13 -0500

    open dialog working

--- src/Application/App.js
+++ src/Application/App.js
@@ -1,7 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
+// const ipc = require('electron').ipcRenderer;
+
+// const selectDirBtn = document.getElementById('select-directory')
+const ipc = window.require("electron").ipcRenderer;
 
 class App extends Component {
+
+  constructor() {
+    super();
+    ipc.on('selected-directory', function (event, path) {
+      console.log("Selected path" + path);
+    });
+
+  }
+
+  openNew = () => {
+        ipc.send('open-file-dialog')
+  }
+
+
+
   render() {
     return (
       <div className="App">
@@ -11,6 +30,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <button type="button" onClick={this.openNew}>Click Me!</button>
       </div>
     );
   }
